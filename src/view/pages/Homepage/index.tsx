14beaf365f535dcf39fe6d08aa54fc5e0cb8f01e
Victor Santos <victords895@gmail.com>
2024-10-11 19:37:55 -0300

    add home-page

---
+++ src/view/pages/Homepage/index.tsx
@@ -0,0 +1,9 @@
+export function Homepage() {
+  return (
+    <>
+      <h1>React App Softplan - Pesquisa de Funcionários</h1>
+      <p>Introdução</p>
+      <p>Funcionalidades</p>
+    </>
+  )
+}
