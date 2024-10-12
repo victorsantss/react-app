e9ad4d3a35a7397763f10128aa7277d163f2f885
Victor Santos <victords895@gmail.com>
2024-10-11 21:30:42 -0300

    refactor: theme provider location

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
 import { Router } from './Router'
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-
-const theme = createTheme({
-  colorSchemes: {
-    dark: true,
-  },
-});
 
 function App() {
-
   return (
-    <ThemeProvider theme={theme}>
-      <Router />
-    </ThemeProvider>
+    <Router />
   )
 }
 
---
+++ src/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  colorSchemes: {
+    dark: true,
+  },
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+  },
+});
