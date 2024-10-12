ccab5c054d86f1667b47bda6ec950c83e0a56e75
Victor Santos <victords895@gmail.com>
2024-10-11 21:34:20 -0300

    refactor: header usage

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Homepage } from "../view/pages/Homepage";
+import { Header } from "../view/components/Header";
 
 export function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/funcionarios" element={<h1>Funcionarios</h1>} />
--- src/view/pages/Homepage/index.tsx
+++ src/view/pages/Homepage/index.tsx
@@ -1,9 +1,6 @@
-import { Header } from "../../components/Header";
-
 export function Homepage() {
   return (
     <>
-      <Header />
       <h1>React App Softplan - Pesquisa de Funcionários</h1>
       <p>Introdução</p>
       <p>Funcionalidades</p>
