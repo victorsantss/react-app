29dc66b0b382a96fc3bc8cf0b38c5095f648239f
Victor Santos <victords895@gmail.com>
2024-10-13 23:01:22 -0300

    refactor: code changed to enable better search with like from json-server

--- src/app/services/employeesService/getAll.ts
+++ src/app/services/employeesService/getAll.ts
@@ -1,11 +1,24 @@
+import { EmployeesFilter } from "../../entities/EmployeesFilter";
 import { httpClient } from "../httpClient";
 
-export type EmployeesFilters = {
-  filterType: string;
-  filterValue: string;
-}
+export async function getAll(filters?: EmployeesFilter) {
+  const {
+    filterType,
+    filterValue
+  } = filters || { filterType: 'nome', filterValue: '' };
 
-export async function getAll(filters?: EmployeesFilters) {
-  const { data } = await httpClient.get('/funcionarios', { params: filters });
+  const filterMap: Record<string, string> = {
+    nome: 'nome_like',
+    filial: 'filial_like',
+    matricula: 'matricula_like',
+  };
+
+  const paramKey = filterMap[filterType];
+
+  const { data } = await httpClient.get('/funcionarios', {
+    params: {
+      [paramKey]: filterValue,
+    },
+  });
   return data;
 }
